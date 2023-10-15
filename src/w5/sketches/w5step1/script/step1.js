a5b6d13bb5880e2e64d5651bbc0771a26a7fc8ce
ljy0813 <143991608+ljy0813@users.noreply.github.com>
2023-10-16 04:01:48 +0900

    a

--- src/w5/sketches/w5step1/script/step1.js
+++ src/w5/sketches/w5step1/script/step1.js
@@ -1,13 +1,12 @@
-const cNum = 8; // 열의 개수
-const rNum = 8; // 행의 개수
-const centerRadius = 100; // 중심 원의 반지름
-const smallRadius = 75; // 작은 원의 반지름 (기존 50에서 75로 증가)
+const cNum = 8;
+const rNum = 8;
+const centerRadius = 100;
+const smallRadius = 75;
 let angleBegin = 0;
 let angleStep;
 let gridC;
 let gridR;
 
-// 각 그래픽의 색상을 저장할 배열
 let colors = [];
 
 function setup() {
@@ -16,14 +15,11 @@ function setup() {
   colorMode(HSL, 360, 100, 100, 100);
   background(360, 0, 100);
 
-  // 각도 증가량 설정
   angleStep = 360 / (60 * 60);
 
-  // 기타 변수 초기화
   gridC = width / cNum;
   gridR = height / rNum;
 
-  // 각 그래픽의 색상 미리 생성
   for (let i = 0; i < cNum * rNum; i++) {
     colors.push(color(random(360), 100, 50, 25));
   }
@@ -52,12 +48,9 @@ function draw() {
     }
   }
 
-  // 각 행의 각도 계산
   for (let r = 0; r < rNum; r++) {
-    // 각 행의 첫 번째 개체를 기준으로 각도 계산
     let angle = angleBegin + r * 15;
 
-    // 각 행의 나머지 개체에 대해 각도 적용
     for (let c = 1; c < cNum; c++) {
       translate(c * gridC + gridC / 2, r * gridR + gridR / 2);
       rotate(angle + c * 15);
