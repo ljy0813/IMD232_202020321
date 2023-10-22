51973d3a00e13c1df5c7a233049190f88a9e0080
ljy0813 <143991608+ljy0813@users.noreply.github.com>
2023-10-22 23:12:51 +0900

    ss

--- src/w6/sketches/w6 step1/script/w6step1.js
+++ src/w6/sketches/w6 step1/script/w6step1.js
@@ -1,46 +1,34 @@
-let particles = [];
-const maxParticles = 100;
 let gravity;
+let emitter;
 
 function setup() {
   setCanvasContainer('canvas', 2, 1, true);
-  colorMode(HSB, 360, 100, 100, 100); // HSB 모드로 변경
-  background(0); // 검은색 배경
+  colorMode(HSL, 360, 100, 100, 100);
+  createCanvas(400, 400);
   gravity = createVector(0, 0.1);
+  emitter = new Emitter(width / 2, height / 2);
+  background(360, 0, 100);
 }
 
 function draw() {
-  console.log('현재 파티클 개수: ' + particles.length);
-  for (let i = particles.length - 1; i >= 0; i--) {
-    let p = particles[i];
-    p.applyForce(gravity);
-    p.update();
-    p.display();
-    if (p.isDead()) {
-      particles.splice(i, 1);
-    }
+  console.log('현재 파티클 개수: ' + emitter.particles.length);
+  for (let i = 0; i < 10; i++) {
+    emitter.createParticle();
   }
+  emitter.update();
+  emitter.display();
 }
 
 function mousePressed() {
-  for (let i = 0; i < maxParticles; i++) {
-    let hue = random(360); // 무작위 색상
-    let angle = random(TWO_PI); // 무작위 방향을 선택
-    let magnitude = 3;
-    let vx = cos(angle) * magnitude;
-    let vy = sin(angle) * magnitude;
-    let p = new Particle(mouseX, mouseY, hue, vx, vy);
-    particles.push(p);
-  }
+  emitter.setLocation(mouseX, mouseY);
 }
 
 class Particle {
-  constructor(x, y, hue, vx, vy) {
+  constructor(x, y) {
     this.position = createVector(x, y);
-    this.velocity = createVector(vx, vy);
-    this.acceleration = createVector();
+    this.velocity = createVector(random(19, 20), random(19, 20));
+    this.acceleration = createVector(0, 0);
     this.lifespan = 60;
-    this.hue = hue;
   }
 
   applyForce(force) {
@@ -56,12 +44,46 @@ class Particle {
   }
 
   display() {
-    fill(this.hue, 100, 100, map(this.lifespan, 0, 60, 0, 100)); // HSB 색상 설정
+    fill(360, 100, 50, map(this.lifespan, 0, 60, 0, 100));
     noStroke();
     ellipse(this.position.x, this.position.y, 10, 10);
   }
 
   isDead() {
     return this.lifespan <= 0 || this.position.y > height;
+  }
+}
+
+class Emitter {
+  constructor(x, y) {
+    this.particles = [];
+    this.location = createVector(x, y);
+  }
+
+  setLocation(x, y) {
+    this.location.x = x;
+    this.location.y = y;
+  }
+
+  createParticle() {
+    let p = new Particle(this.location.x, this.location.y);
+    this.particles.push(p);
+  }
+
+  update() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.applyForce(gravity);
+      p.update();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+
+  display() {
+    for (let p of this.particles) {
+      p.display();
+    }
   }
 }
